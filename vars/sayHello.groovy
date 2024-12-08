d50fdc3dbf9a5ee285571977f1d90d35067939c3
qiang.xie <qiang.xie@riotgames.com>
2024-12-08 17:48:16 +0800

    test parallel

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,4 +1,13 @@
 #!/usr/bin/env groovy
 def call(String name = 'human') {
   echo "Hello, ${name}."
+
+  parallel(
+        a: {
+          echo "This is branch a"
+        },
+        b: {
+          echo "This is branch b"
+        }
+  )
 }
